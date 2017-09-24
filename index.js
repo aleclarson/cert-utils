725240996a84b54f060fe9759b6a70647ec20534
aleclarson <alec.stanford.larson@gmail.com>
2017-09-24 09:23:04 -0400

    Major refactor
    - Add `exports.der` for converting a certificate to DER format
    - Move the `generateCsr` function to `exports.req`

--- index.js
+++ index.js
@@ -7,107 +7,180 @@ var os = require('os');
 
 var DEBUG = /^(1|true)$/.test(process.env.DEBUG);
 
-var optionTypes = {
-  der: 'boolean?',
-  key: 'string|buffer',
-  domain: 'string?',
-  domains: 'string|array?',
-  subject: [{
-    email: 'string?',
-    country: 'string?',
-    state: 'string?',
-    city: 'string?',
-    company: 'string?',
-    division: 'string?',
-  }, '?'],
+exports.der = function certToDer(cert) {
+  assertValid(cert, 'string|buffer');
+
+  var certPath = tempPath();
+  fs.writeFileSync(certPath, cert);
+
+  var derPath = tempPath();
+  return openssl([
+    'x509',
+    '-in', certPath,
+    '-inform', 'PEM',
+    '-out', derPath,
+    '-outform', 'DER',
+  ]).then(function() {
+    var cert = fs.readFileSync(derPath);
+    fs.unlinkSync(certPath);
+    fs.unlinkSync(derPath);
+    return cert;
+  }, function(error) {
+    fs.unlinkSync(certPath);
+    throw error;
+  });
 };
 
-var subjectKeys = {
-  country: 'C',
-  state: 'ST',
-  city: 'L',
-  company: 'O',
-  division: 'OU',
-};
+exports.req = (function() {
+  var optionTypes = {
+    der: 'boolean?',
+    key: 'string|buffer',
+    domain: 'string?',
+    domains: 'string|array?',
+    subject: [{
+      email: 'string?',
+      country: 'string?',
+      state: 'string?',
+      city: 'string?',
+      company: 'string?',
+      division: 'string?',
+    }, '?'],
+  };
 
-module.exports = function generateCsr(options) {
-  assertValid(options, optionTypes);
+  var subjectKeys = {
+    country: 'C',
+    state: 'ST',
+    city: 'L',
+    company: 'O',
+    division: 'OU',
+  };
 
-  var domains = options.domains;
-  if (options.domain) {
-    domains = [options.domain];
-  } else if (typeof domains === 'string') {
-    domains = [domains];
-  }
-  if (!domains) {
-    throw Error('Must define `options.domain` or `options.domains`');
-  }
+  exports.subjectTypes = optionTypes.subject;
 
-  var csrPath = tempPath();
-  var keyPath = tempPath();
+  return function generateCsr(options) {
+    assertValid(options, optionTypes);
 
-  // Store the private key in a temporary file.
-  fs.writeFileSync(keyPath, options.key);
-
-  var args = [
-    'req',
-    '-new',
-    '-nodes',
-    '-out', csrPath,
-    '-key', keyPath,
-  ];
-
-  if (domains.length > 1) {
-    var confPath = tempPath();
-    fs.writeFileSync(confPath, createSSLConfig(confPath, domains, options.subject));
-    args.push('-config', confPath);
-  } else {
-    args.push('-subj', subjectString(domains[0], options.subject));
-  }
-
-  if (DEBUG) {
-    console.log('openssl ' + args.join(' '));
-  }
-  return openssl(args).then(function() {
-    confPath && fs.unlinkSync(confPath);
-    fs.unlinkSync(keyPath);
-
-    // Skip conversion to DER format unless specified.
-    if (!options.der) {
-      var csr = fs.readFileSync(csrPath);
-      fs.unlinkSync(csrPath);
-      return csr;
+    var domains = options.domains;
+    if (options.domain) {
+      domains = [options.domain];
+    } else if (typeof domains === 'string') {
+      domains = [domains];
+    }
+    if (!domains) {
+      throw Error('Must define `options.domain` or `options.domains`');
     }
 
-    var derPath = tempPath();
+    var csrPath = tempPath();
+    var keyPath = tempPath();
+    fs.writeFileSync(keyPath, options.key);
+
     var args = [
       'req',
-      '-in', csrPath,
-      '-out', derPath,
-      '-outform', 'DER',
+      '-new',
+      '-nodes',
+      '-out', csrPath,
+      '-key', keyPath,
     ];
+
+    if (domains.length > 1) {
+      var confPath = tempPath();
+      fs.writeFileSync(confPath, createSSLConfig(confPath, domains, options.subject));
+      args.push('-config', confPath);
+    } else {
+      args.push('-subj', subjectString(domains[0], options.subject));
+    }
 
     if (DEBUG) {
       console.log('openssl ' + args.join(' '));
     }
     return openssl(args).then(function() {
-      var csr = fs.readFileSync(derPath);
-      fs.unlinkSync(csrPath);
-      fs.unlinkSync(derPath);
-      return csr;
+      confPath && fs.unlinkSync(confPath);
+      fs.unlinkSync(keyPath);
+
+      // Skip converting to DER format unless specified.
+      if (!options.der) {
+        var csr = fs.readFileSync(csrPath);
+        fs.unlinkSync(csrPath);
+        return csr;
+      }
+
+      var derPath = tempPath();
+      var args = [
+        'req',
+        '-in', csrPath,
+        '-out', derPath,
+        '-outform', 'DER',
+      ];
+
+      if (DEBUG) {
+        console.log('openssl ' + args.join(' '));
+      }
+      return openssl(args).then(function() {
+        var csr = fs.readFileSync(derPath);
+        fs.unlinkSync(csrPath);
+        fs.unlinkSync(derPath);
+        return csr;
+      }, function(error) {
+        fs.unlinkSync(csrPath);
+        fs.unlinkSync(derPath);
+        throw error;
+      });
     }, function(error) {
-      fs.unlinkSync(csrPath);
-      fs.unlinkSync(derPath);
+      confPath && fs.unlinkSync(confPath);
+      fs.unlinkSync(keyPath);
       throw error;
     });
-  }, function(error) {
-    confPath && fs.unlinkSync(confPath);
-    fs.unlinkSync(keyPath);
-    throw error;
-  });
-};
+  };
 
-module.exports.subjectTypes = optionTypes.subject;
+  function subjectString(domain, options) {
+    var parts = ['/CN=', domain];
+    if (options) {
+      for (var key in subjectKeys) {
+        if (options.hasOwnProperty(key)) {
+          parts.push('/' + subjectKeys[key] + '=' + options[key]);
+        }
+      }
+      if (options.email) {
+        parts.unshift('/emailAddress=', options.email);
+      }
+    }
+    return parts.join('');
+  }
+
+  function createSSLConfig(confPath, domains, options) {
+    var parts = [
+      '[ req ]',
+      'default_bits=2048',
+      'prompt=no',
+      'default_md=sha256',
+      'req_extensions=v3_req',
+      'distinguished_name=dn',
+      '',
+      '[ dn ]',
+      'CN=' + domains[0],
+    ];
+    if (options) {
+      for (var key in subjectKeys) {
+        if (options.hasOwnProperty(key)) {
+          parts.push(subjectKeys[key] + '=' + options[key]);
+        }
+      }
+      if (options.email) {
+        parts.push('emailAddress=' + options.email);
+      }
+    }
+    parts.push(
+      '[ v3_req ]',
+      'subjectAltName=@alt_names',
+      '',
+      '[ alt_names ]'
+    );
+    domains.forEach(function(domain, index) {
+      parts.push('DNS.' + (index + 1) + '=' + domain);
+    });
+    return parts.join('\n');
+  }
+})();
 
 function openssl(args) {
   return new Promise(function(resolve, reject) {
@@ -119,53 +192,4 @@ function openssl(args) {
 
 function tempPath() {
   return path.join(os.tmpdir(), Math.random().toString(16).slice(2));
-}
-
-function subjectString(domain, options) {
-  var parts = ['/CN=', domain];
-  if (options) {
-    for (var key in subjectKeys) {
-      if (options.hasOwnProperty(key)) {
-        parts.push('/' + subjectKeys[key] + '=' + options[key]);
-      }
-    }
-    if (options.email) {
-      parts.unshift('/emailAddress=', options.email);
-    }
-  }
-  return parts.join('');
-}
-
-function createSSLConfig(confPath, domains, options) {
-  var parts = [
-    '[ req ]',
-    'default_bits=2048',
-    'prompt=no',
-    'default_md=sha256',
-    'req_extensions=v3_req',
-    'distinguished_name=dn',
-    '',
-    '[ dn ]',
-    'CN=' + domains[0],
-  ];
-  if (options) {
-    for (var key in subjectKeys) {
-      if (options.hasOwnProperty(key)) {
-        parts.push(subjectKeys[key] + '=' + options[key]);
-      }
-    }
-    if (options.email) {
-      parts.push('emailAddress=' + options.email);
-    }
-  }
-  parts.push(
-    '[ v3_req ]',
-    'subjectAltName=@alt_names',
-    '',
-    '[ alt_names ]'
-  );
-  domains.forEach(function(domain, index) {
-    parts.push('DNS.' + (index + 1) + '=' + domain);
-  });
-  return parts.join('\n');
 }
